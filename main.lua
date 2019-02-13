d49bf2f23feabb97d99eca987f58faee8e810eca
Annamaria Orban <anna.orban@yahoo.com>
2019-02-13 22:05:26 +0000

    making cells alive

--- main.lua
+++ main.lua
@@ -4,11 +4,28 @@ function love.load()
     borderSize = 1
     cellDrawSize = cellSize - borderSize
     gridX, gridY = love.graphics.getWidth() / cellSize, love.graphics.getHeight() / cellSize
+
+    grid = {}
+
+    for y = 1, gridY do
+        grid[y] = {}
+        for x = 1, gridX do
+            grid[y][x] = false
+        end
+    end
+
+-- Temp
+    grid[1][1] = true
+    grid[1][2] = true
 end
 
 function love.update()
     selectedX = math.floor(love.mouse.getX() / cellSize) + 1
     selectedY = math.floor(love.mouse.getY() / cellSize) + 1
+
+    if love.mouse.isDown(1) and selectedX <= gridX and selectedY <= gridY then
+        grid[selectedY][selectedX] = true
+    end
 end
 
 function love.draw()
@@ -16,6 +33,8 @@ function love.draw()
         for x = 1, gridX do
             if x == selectedX and y == selectedY then
                 love.graphics.setColor(0, 1, 1)
+            elseif grid[y][x] then
+                love.graphics.setColor(1, 0, 1)
             else
                 love.graphics.setColor(0.86, 0.86, 0.86)
             end
@@ -29,6 +48,7 @@ function love.draw()
         end
     end
 
+--Temp
     love.graphics.setColor(0, 0, 0)
     love.graphics.print('selected x: '..selectedX..', selected y: '..selectedY)
 end
