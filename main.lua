8f017b320d83604f5d62bd877431e583875f00df
Annamaria Orban <anna.orban@yahoo.com>
2019-02-13 21:42:41 +0000

    select cells

--- main.lua
+++ main.lua
@@ -6,19 +6,31 @@ function love.load()
     gridX, gridY = love.graphics.getWidth() / cellSize, love.graphics.getHeight() / cellSize
 end
 
+function love.update()
+    selectedX = math.floor(love.mouse.getX() / cellSize) + 1
+    selectedY = math.floor(love.mouse.getY() / cellSize) + 1
+end
+
 function love.draw()
     for y = 1, gridY do
         for x = 1, gridX do
-        love.graphics.setColor(0.86, 0.86, 0.86)
-        love.graphics.rectangle(
-            'fill',
-            (x - 1) * cellSize,
-            (y - 1) * cellSize,
-            cellDrawSize,
-            cellDrawSize
-        )
+            if x == selectedX and y == selectedY then
+                love.graphics.setColor(0, 1, 1)
+            else
+                love.graphics.setColor(0.86, 0.86, 0.86)
+            end
+            love.graphics.rectangle(
+                'fill',
+                (x - 1) * cellSize,
+                (y - 1) * cellSize,
+                cellDrawSize,
+                cellDrawSize
+            )
         end
     end
+
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print('selected x: '..selectedX..', selected y: '..selectedY)
 end
 
 function love.keypressed(key)
